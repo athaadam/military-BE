ae169e6fe1dfe2d62b060ed39b60405594317b31
azighaazhar <azighaazhar@student.telkomuniversity.ac.id>
2026-04-10 16:01:59 +0700

    Fix Dotenv and packagejson

--- src/app.js
+++ src/app.js
@@ -1,12 +1,13 @@
+import 'dotenv/config'
 import express from 'express'
 import itemRoutes from './routes/itemRoutes.js'
-import requestRoutes from './routes/requestRoutes.js'
+import requetRoutes from './routes/requetRoutes.js'
 
 const app = express()
 
 app.use(express.json())
 
 app.use('/api/items', itemRoutes)
-app.use('/api/requests', requestRoutes)
+app.use('/api/requests', requetRoutes)
 
 export default app
