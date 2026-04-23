6faaa1602899526563723f8d3880c9bbeebbcd8d
athaadam <154268515+athaadam@users.noreply.github.com>
2026-04-23 15:06:49 +0700

    Revert "Azigha"

--- src/app.js
+++ src/app.js
@@ -1,19 +1,12 @@
-import 'dotenv/config'
 import express from 'express'
 import itemRoutes from './routes/itemRoutes.js'
-import requetRoutes from './routes/requetRoutes.js'
-import authRoutes from './routes/authRoutes.js'
-import userRoutes from './routes/userRoutes.js'
-import returnRoutes from './routes/returnRoutes.js'
+import requestRoutes from './routes/requestRoutes.js'
 
 const app = express()
 
 app.use(express.json())
 
 app.use('/api/items', itemRoutes)
-app.use('/api/requests', requetRoutes)
-app.use('/api/auth', authRoutes)
-app.use('/api/users', userRoutes)
-app.use('/api/returns', returnRoutes)
+app.use('/api/requests', requestRoutes)
 
 export default app
